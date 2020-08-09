7edd40fb3813334b66c9ea59fa17c279666d1830
daianaludice <daianaludice@naver.com>
2020-08-10 04:02:55 +0900

    8/9일까지의 작업, PAGE1,2 완료

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+/* Copyright 2020. Soyoung LEE all rights reserved.
+     Email : daianaludice@naver.com */
+
+// create a standard scene and add it to a controller
+$('#nav').hide();
+$('#page02-1').hide();
+
+$('#button1').click(function(){
+        $('#nav').show();
+});
+
+$('#button2').click(function(){
+        $('#nav').hide();
+});
